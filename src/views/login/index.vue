2147d6afb26ff7c6089b43370e96b308bac117da
evanLee <evanLee@outlook.com>
2024-02-25 23:12:12 +0800

    添加全局样式

---
+++ src/views/login/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="">
+    <el-form class="login-from">
+      <div class="titlr-container">
+        <h3 class="titiel">用户登陆</h3>
+      </div>
+      <!-- username -->
+      <el-form-item>
+        <span class="svg-con">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text"></el-input>
+      </el-form-item>
+      <!-- password -->
+      <el-form-item>
+        <span class="svg-con">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="password" name="password"></el-input>
+        <span class="show-pwd">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+
+      <!-- button -->
+      <el-button type="primary" style="width: 100%; margin-bottom: 20px;">
+        登陆
+      </el-button>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { Avatar } from '@element-plus/icons'
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
